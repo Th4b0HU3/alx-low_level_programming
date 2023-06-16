e538392f0e3e3183ce612d2b576ec6e7b5ded94b
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-15 17:12:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - a program that prints a line with puts function
  * Return: 0 (success)
 */
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void)
+{    puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
