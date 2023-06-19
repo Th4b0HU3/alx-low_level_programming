429c8350e349b82a47cfe7922c5f5eab3ca6c8e4
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-19 11:26:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,1 @@
-#include < stdio.h >
-/**
- * main - Entry point
- * Return: 0 (success)
- */
-int main(void)
-{
-	long long int d;
 
-printf("Size of a char: %u bytes(s)\n", (unsigned long)sizeof(char));
-printf("Size of a int: %u bytes(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %u bytes(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %u bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float int: %u bytes(s)\n", (unsigned long)sizeof(float));
-return (0);
-}
