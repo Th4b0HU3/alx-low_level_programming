19712c28988ac615200f985ef1e127037e08ae85
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-23 17:16:44 -0700

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,9 +4,9 @@
  * main - prints the largest prime factor of 612852475143
  * Return: 0 always
  */
-void main(void)
+int main(void)
 {
-	unsignrd long int i, n = 612852475143;
+	unsigned long int i, n = 612852475143;
 
 	for (i = 3; i < 782849; i = i + 2)
 	{
