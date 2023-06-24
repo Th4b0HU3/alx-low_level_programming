4d90c680380dff8b0a93c4a9ba9e23a7eb5a9677
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-23 17:59:26 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
+		n *= -1;
+		k = n;
+		_putchar('-');
 	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int)n % 10 + '0');
 }
