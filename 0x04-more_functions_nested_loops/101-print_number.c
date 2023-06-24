aa2d119bb5dfb9a1e12863d2f8feee7694c09a6d
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-23 17:39:53 -0700

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - print s an integer
+ * @n: input integer parameter
+ * Return: 0 always
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
