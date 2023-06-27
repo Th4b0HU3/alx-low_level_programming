ad35cdb8e4e9145678a7d3aa74d74709e5b3ba07
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-26 17:45:49 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character of a string
+ * starting with the first character
+ * @*str - input
+ * return: print
+ */
+void puts2(char *str)
+{
+	int long = 0;
+	int t = 0;
+	char *y = str;
+
+	int 0;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	_putchar(str[0]);
+
+	_putchar('\n');
+}
