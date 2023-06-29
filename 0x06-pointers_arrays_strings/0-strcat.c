39bffbabaffdf208ba786e704426bbcc5c98510d
Thabo Lehari <leharithabo.fx@gmail.com>
2023-06-29 12:21:32 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0');
