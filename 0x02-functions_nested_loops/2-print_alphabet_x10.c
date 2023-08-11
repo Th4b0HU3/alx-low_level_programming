399a59c36fc203f30752fae2d73d9f5a6c743b0c
Thabo Lehari <leharithabo.fx@gmail.com>
2023-08-11 00:13:44 -0700

    Update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#inclede "main.h"
+/**
+ * main - 0x02-functions_nested_loops - make alphabet x10
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void);
+{
+
+char ch i8;
+
+int i32;
+
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+}
+{
+_putchar(ch);
+}
+_putchar('\n'); i++
+}
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Cheks for lowercase charactor
+ * Return: 1 for lowercase charactor or 0 for anything else
+ */
+
+int _islower(int c);
+{
+	typedef int c i32;
+
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
